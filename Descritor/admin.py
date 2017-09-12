bfbf8579501551f4e2ed53cddaeff6eb3731151e
falbrito <britofa@paho.org>
2017-09-12 16:04:29 -0300

    Concept proof of DeCS system, first commit

--- Descritor/admin.py
+++ Descritor/admin.py
@@ -16,37 +16,26 @@ from Descritor.forms import DescriptorForm
 
 # usando StackedInline
 class ScopeNoteInline(admin.StackedInline):
+
     model = ScopeNote
     classes = ['collapse']
 
 class AnnotationInline(admin.StackedInline):
+
     model = Annotation
     classes = ['collapse']
 
 
 class DescriptorForm(admin.ModelAdmin):
-    # list_display = ('id_mesh','id_decs')
-    list_display = ('descriptor_en','descriptor_es_la','descriptor_pt','descriptor_es_sp','descriptor_fr')
 
-    # fields = ('id_mesh','id_decs')
-
-    '''
-    fieldsets = (
-        (None, {
-            'fields':('id_mesh','id_decs','descriptor_pt')
-        }),
-        ('Outros campos', {
-            'classes': ('collapse',),
-            'fields': ('descriptor_en','descriptor_es_la','descriptor_es_sp','descriptor_fr'),
-        }),
-    )
-    '''
+    list_display = ('descriptor_en','descriptor_es_la','descriptor_pt','descriptor_es_sp','descriptor_fr','active_descriptor')
 
     form = DescriptorForm
 
 
     # Incluindo o formulario de Descriptor
     inlines = [
+
         ScopeNoteInline,
         AnnotationInline,
     ]
--- Descritor/models.py
+++ Descritor/models.py
@@ -10,17 +10,18 @@ from django.contrib.auth.models import User
 ''' v1, v2, v3, v4, v16 '''
 class Descriptor(models.Model):
 
-    class Meta:
-        verbose_name = u'Descritor'
-        verbose_name_plural = u'Descritores'
-
-    id_mesh = models.CharField( u'ID MESH', max_length=50, null=True, blank=True )
-    id_decs = models.CharField( u'ID DeCS', max_length=50, null=True, blank=True )
     descriptor_en = models.CharField( u'Inglês', max_length=200, null=True, blank=True )
     descriptor_es_la = models.CharField( u'Espanhol', max_length=200, null=True, blank=True )
     descriptor_pt = models.CharField( u'Português', max_length=200, null=True, blank=True )
     descriptor_es_sp = models.CharField( u'Espanhol - Espanha', max_length=200, null=True, blank=True )
     descriptor_fr = models.CharField( u'Francês', max_length=200, null=True, blank=True )
+    active_descriptor = models.BooleanField( u'Ativo', default=True, help_text=u'Marque para definir o Descritor como ativo.' )
+
+    class Meta:
+        verbose_name = u'Descritor'
+        verbose_name_plural = u'Descritores'
+        ordering = ['descriptor_pt']
+
 
     def __unicode__(self): # informação que retornará 
         # return '%s %s' % (self.id, self.descriptor_pt)
@@ -31,16 +32,16 @@ class Descriptor(models.Model):
 ''' v5, v6, v7, v8 '''
 class ScopeNote(models.Model):
 
-    class Meta:
-        verbose_name = u'Definição'
-        verbose_name_plural = u'Definições'
-
     scope_note_en = models.TextField( u'Inglês', max_length=200, null=True, blank=True )
     scope_note_es_la = models.TextField( u'Espanhol', max_length=200, null=True, blank=True )
     scope_note_pt = models.TextField( u'Português', max_length=200, null=True, blank=True )
     scope_note_es_sp = models.TextField( u'Espanhol', max_length=200, null=True, blank=True )
     scope_note_fr = models.TextField( u'Francês', max_length=200, null=True, blank=True )
     id_scope_note = models.OneToOneField(Descriptor)
+
+    class Meta:
+        verbose_name = u'Definição'
+        verbose_name_plural = u'Definições'
 
     def __unicode__(self): # informação que retornará
         return '%s' % (self.id)
@@ -49,16 +50,16 @@ class ScopeNote(models.Model):
 ''' v110, v210 v310 '''
 class Annotation(models.Model):
 
-    class Meta:
-        verbose_name = u'Nota de Indexação'
-        verbose_name_plural = u'Notas de Indexação'
-
     annotation_en = models.TextField( u'Inglês', max_length=200, null=True, blank=True )
     annotation_es_la = models.TextField( u'Espanhol', max_length=200, null=True, blank=True )
     annotation_pt = models.TextField( u'Portugês', max_length=200, null=True, blank=True )
     annotation_es_sp = models.TextField( u'Espanhol - Espanha', max_length=200, null=True, blank=True )
     annotation_fr = models.TextField( u'Francês', max_length=200, null=True, blank=True )
     id_annotation = models.OneToOneField(Descriptor)
+
+    class Meta:
+        verbose_name = u'Nota de Indexação'
+        verbose_name_plural = u'Notas de Indexação'
 
     def __unicode__(self): # informação que retornará
         return '%s' % (self.id)
