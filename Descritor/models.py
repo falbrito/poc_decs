97a58fa495840f7084aa18a6f1310898b3b6d732
falbrito <britofa@paho.org>
2017-09-13 14:15:22 -0300

    Concept proof of DeCS system, first commit

--- Descritor/models.py
+++ Descritor/models.py
@@ -5,17 +5,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
-''' v1, v2, v3, v4, v16 '''
+# Descriptor fields
 class Descriptor(models.Model):
 
-    descriptor_en = models.CharField( u'Inglês', max_length=200, null=True, blank=True )
-    descriptor_es_la = models.CharField( u'Espanhol', max_length=200, null=True, blank=True )
-    descriptor_pt = models.CharField( u'Português', max_length=200, null=True, blank=True )
-    descriptor_es_sp = models.CharField( u'Espanhol - Espanha', max_length=200, null=True, blank=True )
-    descriptor_fr = models.CharField( u'Francês', max_length=200, null=True, blank=True )
     active_descriptor = models.BooleanField( u'Ativo', default=True, help_text=u'Marque para definir o Descritor como ativo.' )
+    # field tag 1
+    descriptor_en = models.CharField( u'Inglês', max_length=200, null=True, blank=True )
+
+    # field tag 2
+    descriptor_es_la = models.CharField( u'Espanhol', max_length=200, null=True, blank=True )
+
+    # field tag 3
+    descriptor_pt = models.CharField( u'Português', max_length=200, null=True, blank=True )
+
+    # field tag 4
+    descriptor_es_sp = models.CharField( u'Espanhol - Espanha', max_length=200, null=True, blank=True )
+
+    # filed tag 16
+    descriptor_fr = models.CharField( u'Francês', max_length=200, null=True, blank=True )
 
     class Meta:
         verbose_name = u'Descritor'
@@ -29,14 +36,24 @@ class Descriptor(models.Model):
         return '%s' % (self.descriptor_pt)
 
 
-''' v5, v6, v7, v8 '''
+# Scope Note
 class ScopeNote(models.Model):
 
-    scope_note_en = models.TextField( u'Inglês', max_length=200, null=True, blank=True )
-    scope_note_es_la = models.TextField( u'Espanhol', max_length=200, null=True, blank=True )
-    scope_note_pt = models.TextField( u'Português', max_length=200, null=True, blank=True )
-    scope_note_es_sp = models.TextField( u'Espanhol', max_length=200, null=True, blank=True )
-    scope_note_fr = models.TextField( u'Francês', max_length=200, null=True, blank=True )
+    # field tag 5
+    scope_note_en = models.TextField( u'Inglês', max_length=3000, null=True, blank=True )
+
+    # field tag 6
+    scope_note_es_la = models.TextField( u'Espanhol', max_length=3000, null=True, blank=True )
+
+    # field tag 7
+    scope_note_pt = models.TextField( u'Português', max_length=3000, null=True, blank=True )
+
+    # field tag 8
+    scope_note_es_sp = models.TextField( u'Espanhol', max_length=3000, null=True, blank=True )
+
+    # field tag not exist before
+    scope_note_fr = models.TextField( u'Francês', max_length=3000, null=True, blank=True )
+
     id_scope_note = models.OneToOneField(Descriptor)
 
     class Meta:
@@ -47,14 +64,24 @@ class ScopeNote(models.Model):
         return '%s' % (self.id)
 
 
-''' v110, v210 v310 '''
+# Annotation
 class Annotation(models.Model):
 
+    # field tag 110
     annotation_en = models.TextField( u'Inglês', max_length=200, null=True, blank=True )
+
+    # field tag 210
     annotation_es_la = models.TextField( u'Espanhol', max_length=200, null=True, blank=True )
+
+    # field tag 310
     annotation_pt = models.TextField( u'Portugês', max_length=200, null=True, blank=True )
+
+    # field tag not exist before
     annotation_es_sp = models.TextField( u'Espanhol - Espanha', max_length=200, null=True, blank=True )
+
+    # field tag not exist before
     annotation_fr = models.TextField( u'Francês', max_length=200, null=True, blank=True )
+
     id_annotation = models.OneToOneField(Descriptor)
 
     class Meta:
